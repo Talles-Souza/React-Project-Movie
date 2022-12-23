457f66440d58d0ff4b18ebea9d5a10f78c6c4a0a
Talles-Souza <tallesdelfino1@gmail.com>
2022-12-23 17:54:26 -0300

    created css styled page home

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import api from "../../services/apiConnect";
 import { Link } from "react-router-dom";
+import "./style.css";
 
 function Home() {
   const [movies, setMovies] = useState([]);
