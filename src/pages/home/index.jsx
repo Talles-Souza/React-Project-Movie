27f8cc3f7833df10227f92f7cbe32cbc37ec99e5
Talles-Souza <tallesdelfino1@gmail.com>
2022-12-23 15:45:27 -0300

    created request movies

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,7 +1,43 @@
+import { useEffect, useState } from "react";
+import api from "../../services/apiConnect";
+import { Link } from "react-router-dom";
+
 function Home() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function loadMovies() {
+      const response = await api.get("movie/now_playing", {
+        params: {
+          api_key: "2bd73a1735fb2df61a4711aaa922abb1",
+          language: "pt-BR",
+          page: 1,
+        },
+      });
+
+      setMovies(response.data.results.slice(0, 10));
+      //setLoading(false);
+    }
+
+    loadMovies();
+  }, []);
+
   return (
-    <div>
-      <h1>Bem-vindo a home</h1>
+    <div className="container">
+      <div className="lista-filmes">
+        {movies.map((movie) => {
+          return (
+            <article key={movie.id}>
+              <strong>{movie.title}</strong>
+              <img
+                src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+                alt={movie.title}
+              />
+              <Link to={`/filme/${movie.id}`}>Acessar</Link>
+            </article>
+          );
+        })}
+      </div>
     </div>
   );
 }
--- src/routes.jsx
+++ src/routes.jsx
@@ -2,10 +2,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/home/index";
 import Movie from "./pages/movie/index";
-
+import Header from "./components/header";
 function RoutesApp() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/movie" element={<Movie />} />
