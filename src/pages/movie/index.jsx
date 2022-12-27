29498542d900da1ee45611630223e2857c966beb
Talles-Souza <tallesdelfino1@gmail.com>
2022-12-27 09:19:45 -0300

    finish

--- src/pages/movie/index.jsx
+++ src/pages/movie/index.jsx
@@ -1,11 +1,35 @@
-import "./style.css";
-import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import api from "../../services/apiConnect";
+
 function Movie() {
   const { id } = useParams();
-  console.log(id);
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadMovie() {
+      await api
+        .get(`/movie/${id}`, {
+          params: {
+            api_key: "2bd73a1735fb2df61a4711aaa922abb1",
+            language: "pt-BR",
+          },
+        })
+        .then((response) => {
+          console.log(response);
+          setFilme(response.data);
+        })
+        .catch(() => {
+          console.log("FILME NAO ENCONTRADO");
+        });
+    }
+    loadMovie();
+  }, [id]);
+
   return (
     <div>
-      <h1>Bem-vindo a pagina de filmes </h1>
+      <h1>Bem-vindo a pagina de filmes {id} </h1>
     </div>
   );
 }
